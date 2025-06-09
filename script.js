560bf1a08995634ce5299407a772006702e455e1
Om Kumar <omk152004@gmail.com>
2025-06-10 02:58:42 +0530

    slider change and  footer

---
+++ script.js
@@ -0,0 +1,68 @@
+const images = [
+  'https://lnct.ac.in/wp-content/uploads/2024/10/LNCT-Garba-Day-1-31.jpeg',
+  'https://lnct.ac.in/wp-content/uploads/2024/09/Induction-Programme-for-our-B-Tech-I-Year-students.jpg',
+  'https://lnct.ac.in/wp-content/uploads/2024/10/LNCTU-28.jpeg',
+  'https://lnct.ac.in/wp-content/uploads/2024/10/LNCTU-8.jpeg',
+  'https://lnct.ac.in/wp-content/uploads/2024/10/LNCT-Garba-Day-2-9.jpeg',
+  'https://lnct.ac.in/wp-content/uploads/2023/05/LifeatLNCT-14.jpg',
+  'https://lnct.ac.in/wp-content/uploads/2023/05/LifeatLNCT-10.jpg',
+  'https://lnct.ac.in/wp-content/uploads/2023/05/LifeatLNCT-16.jpg',
+  'https://lnct.ac.in/wp-content/uploads/2024/10/LNCT-Garba-Day-3-50.jpeg'
+];
+
+let currentIndex = 0;
+const slider = document.getElementById('gallery-slider');
+
+
+images.forEach((src, i) => {
+  const img = document.createElement('img');
+  img.src = src;
+  slider.appendChild(img);
+});
+
+const imgElements = slider.querySelectorAll('img');
+
+function updateClasses() {
+  imgElements.forEach((img, i) => {
+    img.className = ''; // reset class
+
+    if (i === currentIndex) {
+      img.classList.add('center');
+    } else if (i === (currentIndex - 1 + images.length) % images.length) {
+      img.classList.add('left');
+    } else if (i === (currentIndex + 1) % images.length) {
+      img.classList.add('right');
+    } else if (i === (currentIndex - 2 + images.length) % images.length) {
+      img.classList.add('hidden-left');
+    } else if (i === (currentIndex + 2) % images.length) {
+      img.classList.add('hidden-right');
+    } else {
+      // Hide completely offscreen ones (optional)
+      img.classList.add('hidden-right');
+    }
+  });
+}
+
+
+function nextImage() {
+  currentIndex = (currentIndex + 1) % images.length;
+  updateClasses();
+}
+
+function prevImage() {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  updateClasses();
+}
+
+document.getElementById('next').addEventListener('click', nextImage);
+document.getElementById('prev').addEventListener('click', prevImage);
+
+// Auto slide
+setInterval(nextImage, 3000);
+
+// Initial render
+updateClasses();
+
+
+
+
