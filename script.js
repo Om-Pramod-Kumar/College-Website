5898c31beebf1da23cdb169002e090ade6131fbe
Om Kumar <omk152004@gmail.com>
2025-06-19 23:09:24 +0530

    Responsive

--- script.js
+++ script.js
@@ -1,65 +1,81 @@
-const images = [
-  'https://lnct.ac.in/wp-content/uploads/2024/10/LNCT-Garba-Day-1-31.jpeg',
-  'https://lnct.ac.in/wp-content/uploads/2024/09/Induction-Programme-for-our-B-Tech-I-Year-students.jpg',
-  'https://lnct.ac.in/wp-content/uploads/2024/10/LNCTU-28.jpeg',
-  'https://lnct.ac.in/wp-content/uploads/2024/10/LNCTU-8.jpeg',
-  'https://lnct.ac.in/wp-content/uploads/2024/10/LNCT-Garba-Day-2-9.jpeg',
-  'https://lnct.ac.in/wp-content/uploads/2023/05/LifeatLNCT-14.jpg',
-  'https://lnct.ac.in/wp-content/uploads/2023/05/LifeatLNCT-10.jpg',
-  'https://lnct.ac.in/wp-content/uploads/2023/05/LifeatLNCT-16.jpg',
-  'https://lnct.ac.in/wp-content/uploads/2024/10/LNCT-Garba-Day-3-50.jpeg'
-];
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+let open = false;
 
-let currentIndex = 0;
-const slider = document.getElementById('gallery-slider');
-
-
-images.forEach((src, i) => {
-  const img = document.createElement('img');
-  img.src = src;
-  slider.appendChild(img);
+hamburger.addEventListener('click', () => {
+  open = !open;
+  navLinks.classList.toggle('show');
+  hamburger.innerHTML = open ? '✕' : `
+    <div class="bar"></div>
+    <div class="bar"></div>
+    <div class="bar"></div>`;
 });
 
-const imgElements = slider.querySelectorAll('img');
+ const images = [
+      'https://lnct.ac.in/wp-content/uploads/2024/10/LNCT-Garba-Day-1-31.jpeg',
+      'https://lnct.ac.in/wp-content/uploads/2024/09/Induction-Programme-for-our-B-Tech-I-Year-students.jpg',
+      'https://lnct.ac.in/wp-content/uploads/2024/10/LNCTU-28.jpeg',
+      'https://lnct.ac.in/wp-content/uploads/2024/10/LNCTU-8.jpeg',
+      'https://lnct.ac.in/wp-content/uploads/2024/10/LNCT-Garba-Day-2-9.jpeg',
+      'https://lnct.ac.in/wp-content/uploads/2023/05/LifeatLNCT-14.jpg',
+      'https://lnct.ac.in/wp-content/uploads/2023/05/LifeatLNCT-10.jpg',
+      'https://lnct.ac.in/wp-content/uploads/2023/05/LifeatLNCT-16.jpg',
+      'https://lnct.ac.in/wp-content/uploads/2024/10/LNCT-Garba-Day-3-50.jpeg'
+    ];
 
-function updateClasses() {
-  imgElements.forEach((img, i) => {
-    img.className = '';
+    let currentIndex = 0;
+    const slider = document.getElementById('gallery-slider');
 
-    if (i === currentIndex) {
-      img.classList.add('center');
-    } else if (i === (currentIndex - 1 + images.length) % images.length) {
-      img.classList.add('left');
-    } else if (i === (currentIndex + 1) % images.length) {
-      img.classList.add('right');
-    } else if (i === (currentIndex - 2 + images.length) % images.length) {
-      img.classList.add('hidden-left');
-    } else if (i === (currentIndex + 2) % images.length) {
-      img.classList.add('hidden-right');
-    } else {
-      img.classList.add('hidden-right');
+    images.forEach((src) => {
+      const img = document.createElement('img');
+      img.src = src;
+      slider.appendChild(img);
+    });
+
+    const imgElements = slider.querySelectorAll('img');
+
+    function updateClasses() {
+      imgElements.forEach((img, i) => {
+        img.className = '';
+        if (i === currentIndex) {
+          img.classList.add('center');
+        } else if (i === (currentIndex - 1 + images.length) % images.length) {
+          img.classList.add('left');
+        } else if (i === (currentIndex + 1) % images.length) {
+          img.classList.add('right');
+        } else if (i === (currentIndex - 2 + images.length) % images.length) {
+          img.classList.add('hidden-left');
+        } else if (i === (currentIndex + 2) % images.length) {
+          img.classList.add('hidden-right');
+        } else {
+          img.classList.add('hidden-right');
+        }
+      });
     }
-  });
-}
 
+    function nextImage() {
+      currentIndex = (currentIndex + 1) % images.length;
+      updateClasses();
+    }
 
-function nextImage() {
-  currentIndex = (currentIndex + 1) % images.length;
-  updateClasses();
-}
+    function prevImage() {
+      currentIndex = (currentIndex - 1 + images.length) % images.length;
+      updateClasses();
+    }
 
-function prevImage() {
-  currentIndex = (currentIndex - 1 + images.length) % images.length;
-  updateClasses();
-}
+    document.getElementById('next').addEventListener('click', nextImage);
+    document.getElementById('prev').addEventListener('click', prevImage);
 
-document.getElementById('next').addEventListener('click', nextImage);
-document.getElementById('prev').addEventListener('click', prevImage);
+    let startX = 0;
+    slider.addEventListener('touchstart', (e) => startX = e.touches[0].clientX);
+    slider.addEventListener('touchend', (e) => {
+      const diff = e.changedTouches[0].clientX - startX;
+      if (diff > 50) prevImage();
+      else if (diff < -50) nextImage();
+    });
 
-
-setInterval(nextImage, 3000);
-
-updateClasses();
+    setInterval(nextImage, 3000);
+    updateClasses();
 
 
 ///-------flip  code-------------////\
